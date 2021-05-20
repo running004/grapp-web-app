52869a15121e26b13435320f1061392a4d82c866
sertapia <sertapia@ucm.es>
2021-05-20 13:53:02 +0200

    Añadido clase test busqueda prenda

---
+++ src/test/java/grapp/grapp/BusquedaPrendaTest.java
@@ -0,0 +1,11 @@
+package grapp.grapp;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BusquedaPrendaTest {
+    
+    // GR1.2
+    //prueba de buscar prenda por nombre
+
+}
